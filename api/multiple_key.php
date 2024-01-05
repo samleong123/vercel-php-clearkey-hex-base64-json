cf196df16f191ef80539686e66fb9bce13f968bc
Sam Sam <58818070+samleong123@users.noreply.github.com>
2024-01-05 09:34:16 +0800

    Update multiple_key.php

--- api/multiple_key.php
+++ api/multiple_key.php
@@ -2,16 +2,17 @@
 /* error_reporting(0);
 $error = 2; */
 $data = $_REQUEST["data"];
-echo base64_decode($data);
 $json_data = json_decode(base64_decode($data),true);
 $keys_array = [];
 for ($i = 0; $i < count($json_data); $i++) {
 
-$bin = hex2bin($json_data[$i][key_id]);
+$hex = $json_data[$i][key_id]
+$bin = hex2bin($hex);
 $keyid64 = base64_encode($bin);
 $finalkeyid64 = str_replace('=', '', $keyid64);
 
-$bin2 = hex2bin($json_data[$i][key]);
+$hex2 = $json_data[$i][key]
+$bin2 = hex2bin($hex2);
 $key64 = base64_encode($bin2);
 $finalkey64 = str_replace('=', '', $key64);
 
