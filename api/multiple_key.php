2557991ea47c9b57b055950c7e8aefedf24036dd
Sam Sam <58818070+samleong123@users.noreply.github.com>
2024-01-05 09:25:40 +0800

    Create multiple_key.php

---
+++ api/multiple_key.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(0);
+$error = 2;
+$data = $_REQUEST["data"];
+$json_data = json_decode(base64_decode($data),true);
+$keys_array = [];
+for ($i = 0; $i < count($json_data); $i++) {
+
+$bin = hex2bin($json_data[$i][key_id]);
+$keyid64 = base64_encode($bin);
+$finalkeyid64 = str_replace('=', '', $keyid64);
+
+$bin2 = hex2bin($json_data[$i][key]);
+$key64 = base64_encode($bin2);
+$finalkey64 = str_replace('=', '', $key64);
+
+    $key = [
+        "kty" => "oct",
+        "kid" => $finalkeyid64,
+        "k"   => $finalkey64
+    ];
+    $keys_array[] = $key;
+}
+
+$output_array = ["keys" => $keys_array];
+$output_json = json_encode($output_array, JSON_PRETTY_PRINT);
+header("Content-Type: application/json");
+echo $output_json;
+?>
